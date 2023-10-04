6f17d3792f2f8937edf619d16d3636b90c8d5102
bobbychansfu <bobbychansfu@gmail.com>
2023-10-04 11:43:32 -0700

    js - objects and functions

--- 4/javascript/js/script1.js
+++ 4/javascript/js/script1.js
@@ -2,8 +2,46 @@
 // window.alert("Hi " + uname)
 
 
-function myfunc(name){
-    console.log("Hello " + name + " !")
+// myfunc2("Bobby")
+
+// var myfunc2 = function(name) {
+//     console.log("Hello " + name + "!")
+// }
+
+// function myfunc(name){
+//     console.log("Hello " + name + " !")
+//     innerfunction()
+
+//     function innerfunction(){
+//         console.log("inner function!")
+//     }
+// }
+
+// callbacks
+/*
+var i = 1
+
+function process(){
+    console.log("hello " + i)
+    i++
 }
 
-myfunc("Bobby")
+var inter = window.setInterval(process, 1000)
+*/
+
+// Objects
+var person = {
+    name: 'bobby',
+    age: 42,
+    email: 'bobbyc@sfu.ca',
+    address: {
+        street: 'fake ave',
+        number: 123
+    },
+    fullname: function(){
+        return this.name + " " + this.age
+    }
+}
+
+var arr = [1,2,3,4,5,person]
+arr[5].address.street
