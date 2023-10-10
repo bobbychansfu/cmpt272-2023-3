54644108f042c269dcdb9f8e57ff1c69d671b03e
bobbychansfu <bobbychansfu@gmail.com>
2023-10-10 11:26:09 -0700

    js: objects, selectors, events

--- 4/javascript/js/script1.js
+++ 4/javascript/js/script1.js
@@ -1,23 +1,26 @@
 // var uname = window.prompt("Tell me your name", "Stan")
 // window.alert("Hi " + uname)
 
+/******* Functions */ 
 
-// myfunc2("Bobby")
+/*
+var myfunc2 = function(name) {
+    console.log("Hello " + name + "!")
+}
+myfunc2("Bobby")
 
-// var myfunc2 = function(name) {
-//     console.log("Hello " + name + "!")
-// }
+function myfunc(name){
+    console.log("Hello " + name + " !")
+    innerfunction()
 
-// function myfunc(name){
-//     console.log("Hello " + name + " !")
-//     innerfunction()
+    function innerfunction(){
+        console.log("inner function!")
+    }
+}
 
-//     function innerfunction(){
-//         console.log("inner function!")
-//     }
-// }
+*/
 
-// callbacks
+/********* Callbacks */ 
 /*
 var i = 1
 
@@ -29,7 +32,9 @@ function process(){
 var inter = window.setInterval(process, 1000)
 */
 
-// Objects
+/********* Objects */ 
+
+/*
 var person = {
     name: 'bobby',
     age: 42,
@@ -42,6 +47,50 @@ var person = {
         return this.name + " " + this.age
     }
 }
+*/
 
-var arr = [1,2,3,4,5,person]
-arr[5].address.street
+/********* Constructors */
+
+function Person(n,a,e) {
+    this.name = n
+    this.age = a
+    this.email = e
+}
+
+var p1 = new Person('Bobby',42,'bobbyc@sfu.ca')
+p1.address = {street: 'fake ave',number: 123}
+var p2 = new Person('Sarah',42,'sarah@sfu.ca')
+
+Person.prototype.fullname = function(){
+    return this.name + " " + this.age
+}
+
+
+/********* Arrays and Collections */ 
+
+// var arr = [1,2,3,4,5,person]
+// arr[5].address.street
+
+/********** Adding to the DOM */
+
+/*
+var output = document.getElementById("output")
+var content = document.createTextNode("hello world")
+var p = document.createElement("p")
+p.appendChild(content)
+output.appendChild(p)
+*/
+
+
+
+// output -> p -> hello world
+
+/********** Events */
+
+function clicking(event){
+    event.preventDefault()
+    console.log(event.clientX + " " + event.clientY)
+}
+
+var button = document.getElementById("sub")
+button.onclick = clicking
